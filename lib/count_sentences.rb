9d5a052a930d4468c3e8d58212ad5666a47673cc
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-10-27 06:40:52 +0300

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,25 @@ require 'pry'
 
 class String
 
+  attr_accessor :string
+
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?('!')
 
   end
 
   def count_sentences
+    self.split(/(?<=[.!?]).(?=[a-zA-Z])/).count
 
   end
 end
